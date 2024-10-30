ad8ed14271828a74e512584f627a8c0c9cb6c7bb
Долгов Роман Андреевич <rad2312@mail.ru>
2024-10-30 17:23:18 +0300

    Update DoubleDeserializer.java

--- src/main/java/ru/mirea/dolgov/stonks/util/DoubleDeserializer.java
+++ src/main/java/ru/mirea/dolgov/stonks/util/DoubleDeserializer.java
@@ -1,4 +1,20 @@
 package ru.mirea.dolgov.stonks.util;
 
-public class DoubleDeserializer {
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+import java.io.IOException;
+
+public class DoubleDeserializer extends JsonDeserializer<Double> {
+    @Override
+    public Double deserialize(JsonParser parser, DeserializationContext ctxt) throws IOException {
+        String doubleString = parser.getText();
+        if (doubleString.contains(",")) {
+            doubleString = doubleString.replace(',', '.');
+        }
+        return Double.valueOf(doubleString);
+    }
 }
+

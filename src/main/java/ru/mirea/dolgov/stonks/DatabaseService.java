269ca37f55f56a71238cdca2fd2a78d8381717fb
Долгов Роман Андреевич <rad2312@mail.ru>
2024-10-30 17:20:25 +0300

    Update DatabaseService.java

--- src/main/java/ru/mirea/dolgov/stonks/DatabaseService.java
+++ src/main/java/ru/mirea/dolgov/stonks/DatabaseService.java
@@ -1,4 +1,12 @@
 package ru.mirea.dolgov.stonks;
 
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+
 public interface DatabaseService {
+    Valute getValuteOfDate(LocalDate date) throws SQLException;
+
+    void saveMaxValuteOfDate(String fio, Valute valute, LocalDate date) throws SQLException;
 }
+

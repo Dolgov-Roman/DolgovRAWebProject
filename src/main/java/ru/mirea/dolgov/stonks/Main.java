3c561dc16ebd028449a979b7191c3f9a95f1d9b9
Долгов Роман Андреевич <rad2312@mail.ru>
2024-10-30 17:21:07 +0300

    Update Main.java

--- src/main/java/ru/mirea/dolgov/stonks/Main.java
+++ src/main/java/ru/mirea/dolgov/stonks/Main.java
@@ -1,4 +1,21 @@
 package ru.mirea.dolgov.stonks;
 
+import ru.mirea.dolgov.stonks.StonksClient;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.sql.SQLException;
+
 public class Main {
+    public static void main(String[] args) {
+        try {
+            StonksClient client = new StonksClient();
+            LocalDate dateOfBirth = LocalDate.of(2004, 12, 23); // Замените на вашу дату рождения
+            client.fetchAndSaveMaxCurrency(dateOfBirth);
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+

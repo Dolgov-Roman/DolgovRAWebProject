ba02388d433dd53d93cf27c3eeb5f73c8073e30a
Долгов Роман Андреевич <rad2312@mail.ru>
2024-10-30 17:20:03 +0300

    Update DailyCurs.java

--- src/main/java/ru/mirea/dolgov/stonks/DailyCurs.java
+++ src/main/java/ru/mirea/dolgov/stonks/DailyCurs.java
@@ -1,4 +1,44 @@
 package ru.mirea.dolgov.stonks;
 
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.Date;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class DailyCurs {
+
+    @JacksonXmlProperty(localName = "Date", isAttribute = true)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yyyy")
+    private Date date;
+
+    @JacksonXmlProperty(localName = "name", isAttribute = true)
+    private String name;
+
+    @JacksonXmlProperty(localName = "Valute")
+    @JacksonXmlElementWrapper(useWrapping = false)
+    private List<Valute> valutes;
+
+    public DailyCurs() {}
+
+    public DailyCurs(Date date, String name, List<Valute> valutes) {
+        this.date = date;
+        this.name = name;
+        this.valutes = valutes;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Valute> getValutes() {
+        return valutes;
+    }
 }
